f0e742a664ea8b0b5323478c0ab036026a65f8db
draeriel <danieljuanbarbosa@gmail.com>
2018-04-19 17:34:51 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+	public static void main (String[] args) {
+
+		Persona madre = new Persona.Builder("Maria")
+                .setMunicipio("Selva")
+                .setMayor(37)
+                .setLugarTrabajo("Google")
+                .build();
+
+		// este codigo debe compilar
+		/*
+		Persona hijo = new Persona.Builder("Pedro")
+                .setMenor(4)
+                .setColegio("Colegio de Selva")
+                .build();
+		*/
+		// esta codigo NO debe compilar
+		/*
+		Persona mal = new Persona.Builder("Luisa")
+				    .setMayor(20)
+				    .setColegio("Colegio de Villa Arriba")
+				    .build();
+        */
+	}
+}
